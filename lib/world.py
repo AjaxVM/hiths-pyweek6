fbf2f3a61c82be340451b952e31fe0baf7c24761
RoeBros <RoeBros@a921e572-4749-0410-9c32-5fb0c26aa395>
2008-03-31 18:43:38 +0000

    Territories are now bordered - so we don't have to use the random color mess we had before - and is still there to show a point - but easily removable.

--- lib/world.py
+++ lib/world.py
@@ -3,6 +3,8 @@ import time
 import pygame
 from pygame.locals import *
 
+import util
+
 class Actor(object):
     def __init__(self, image, pos = (0.5, 0.5)):
         """0.5 = middle of tile ;)
@@ -15,10 +17,8 @@ class Actor(object):
 
 class MapGrid(object):
     def __init__(self, #images={"n":None},
-                 colors={"-":None},
                  grid = [[]]):
 ##        self.images = images
-        self.colors = colors
         self.grid = grid
 
     def get_dimensions(self):
@@ -26,7 +26,7 @@ class MapGrid(object):
 
 
 class World(object):
-    def __init__(self, surface, tile_size=(15, 10),
+    def __init__(self, surface, tile_size=(30, 15),
                  map_grid=None, background=None):
 
         self.tile_size = tile_size
@@ -37,10 +37,10 @@ class World(object):
 
 
         self.grid = map_grid
-        self.center = (self.display.get_width()/2,
-                       self.display.get_height()/2)
-        self.center_view = self.center
+        self.offset = [0,0]
         self.map_size = ()
+        self.territories = []
+        self.terr_points = []
 
         self.__images = {}
         self.actors = []
@@ -49,13 +49,22 @@ class World(object):
         self.__images = {}
         for i in self.actors:
             self.__images[i.image] = pygame.image.load(i.image).convert_alpha()
-####        for i in self.grid.images:
-####            self.__images[i] = pygame.image.load(
-####                                self.grid.images[i]).convert_alpha()
         for i in more:
             self.__images[i] = pygame.image.load(i).convert_alpha()
 
         return None
+
+    def __fix_offset(self):
+        if self.offset[0] < 0:
+            self.offset[0] = 0
+        if self.offset[0] > self.tile_size[0] * len(self.grid.grid[0]) - self.display.get_width():
+            self.offset[0] = self.tile_size[0] * len(self.grid.grid[0]) - self.display.get_width()
+
+        if self.offset[1] < 0:
+            self.offset[1] = 0
+        if self.offset[1] > self.tile_size[1] * len(self.grid.grid) - self.display.get_height():
+            self.offset[1] = self.tile_size[1] * len(self.grid.grid) - self.display.get_height()
+        pass
 
     def render(self):
         if self.map_size == ():
@@ -63,6 +72,8 @@ class World(object):
             x *= self.tile_size[0]
             y *= self.tile_size[1]
             self.map_size = x, y
+            self.territories = util.get_territories(self.grid.grid)
+            self.terr_points = [util.get_points(i) for i in self.territories]
 
         if self.background:
             self.display.blit(self.background, (0,0))
@@ -71,29 +82,40 @@ class World(object):
 
 
         tx, ty = self.tile_size
-        dx, dy = (self.center_view[0] - self.center[0],
-                  self.center_view[1] - self.center[1])
-        if dx < 0:
-            dx = 0
-        if dy < 0:
-            dy = 0
 
-        if dx > self.map_size[0]:
-            dx = self.map_size[0]
-        if dy > self.map_size[1]:
-            dy = self.map_size[1]
+        self.__fix_offset()
+
+        dx, dy = self.offset
 
         ypos = 0
-        col = self.grid.colors
+        col = {1: (255, 0, 0),
+                    2: (0, 255, 0),
+                    3: (0, 0, 255),
+                    4: (255, 255, 0),
+                    5: (255, 0, 255),
+                    6: (0, 255, 255),
+                  0:(0,125,0)}
         for y in self.grid.grid:
             xpos = 0
             for x in y:
-                if col[x]:
-                    r = (xpos * tx - dx, ypos * ty - dy,
-                         tx, ty)
-                    pygame.draw.rect(self.display, col[x], r)
+                r = (xpos * tx - dx, ypos * ty - dy,
+                     tx, ty)
+                if x == 0:
+                    pygame.draw.rect(self.display, (125, 125, 125), r)
+                else:
+                    pygame.draw.rect(self.display, col[x%7], r)
                 xpos += 1
             ypos += 1
+
+        #render borders between territories...
+        for i in self.terr_points:
+            for x in i:
+                pygame.draw.line(self.display, [255,255,255],
+                                 [x[0][0]*self.tile_size[0]-self.offset[0],
+                                  x[0][1]*self.tile_size[1]-self.offset[1]],
+                                 [x[1][0]*self.tile_size[0]-self.offset[0],
+                                  x[1][1]*self.tile_size[1]-self.offset[1]],
+                                 1)
 
         for i in self.actors:
             self.display.blit(img[i.image], (i.pos[0] * tx - dx,
@@ -101,17 +123,10 @@ class World(object):
 
     def get_mouse_pos(self):
         p = pygame.mouse.get_pos()
-        dx, dy = (self.center_view[0] - self.center[0],
-                  self.center_view[1] - self.center[1])
-        if dx < 0:
-            dx = 0
-        if dy < 0:
-            dy = 0
 
-        if dx > self.map_size[0]:
-            dx = self.map_size[0]
-        if dy > self.map_size[1]:
-            dy = self.map_size[1]
+        self.__fix_offset()
+
+        dx, dy = self.offset
 
         x = (p[0] + dx) / self.tile_size[0]
         y = (p[1] + dy) / self.tile_size[1]
