5cd8bc25277bdbd4f81f43ab126a90f37886bef0
RoeBros <RoeBros@a921e572-4749-0410-9c32-5fb0c26aa395>
2008-04-02 20:50:19 +0000

    Basic game-play done - needs ui improvements though. You will generate new units at your capitols every turn, you can attack or move units between adjacent territories. When you are the last player the game tells you that you won :D

--- trunk/lib/main.py
+++ trunk/lib/main.py
@@ -4,6 +4,8 @@ from world import *
 import util
 import rules
 
+import gui, wui
+
 import random
 
 import time, os
@@ -48,22 +50,28 @@ def make_map_players(world):
 def main():
     pygame.init()
     screen = pygame.display.set_mode((640, 480))
-##    world_screen = pygame.Surface((640, 300))
     world_screen = screen.subsurface((0, 0, 640, 320))
+
+    app = gui.App(screen, background_color=None)
+    app.theme = gui.make_theme(os.path.join("data", "gui"))
+    app.always_render = True
+    end_turn_button = gui.Button(app, (-1, 480), "End Turn", "End Turn",
+                                widget_pos="bottomleft")
+    whos_turn_label = gui.Label(app, (-1, 480), "WT Label", "It is player 1's turn",
+                          widget_pos="bottomright")
+    whos_turn_label.theme.label["text-color"] = [255,0,0]
+    whos_turn_label.make_image()
 
     mg = MapGrid(util.make_random_map())
 
-##    p = Player(mg.comp_terrs[0], (255, 0, 0))
-##    p.territories = mg.comp_terrs
-
     world = World(world_screen, map_grid=mg)
-##    world.players = [p]
     make_map_players(world)
 
     picktwo = []
+    whos_turn = 0
 
     while 1:
-        for event in pygame.event.get():
+        for event in app.get_events():
             if event.type == QUIT:
                 pygame.quit()
                 return
@@ -75,12 +83,8 @@ def main():
                                                            "screenie %s.jpg"%a))
 
                 if event.key == K_SPACE:
-##                    world.map_size=()
                     mg = MapGrid(util.make_random_map())
                     world.grid = mg
-##                    p = Player(mg.comp_terrs[0], (255, 0, 0))
-##                    p.territories = mg.comp_terrs
-##                    world.players = [p]
                     make_map_players(world)
 
             if event.type == MOUSEBUTTONDOWN:
@@ -89,38 +93,76 @@ def main():
                     if x:
                         print "clicked player #%ss territory: %s"%(x[0]+1, x[1])
                         picktwo.append(x)
+
+            if event.type == gui.GUI_EVENT:
+                if event.widget == gui.Button and event.name == "End Turn":
+                    if event.action == gui.GUI_EVENT_CLICK:
+                        world.players[whos_turn].end_turn()
+                        whos_turn += 1
+                        if whos_turn >= len(world.players):
+                            whos_turn = 0
+                        whos_turn_label.text = "It is player %ss turn"%(whos_turn+1)
+                        whos_turn_label.theme.label["text-color"] = world.players[whos_turn].color
+                        whos_turn_label.make_image()
+
+        if world.players[whos_turn].dead:
+            whos_turn += 1
+            if whos_turn >= len(world.players):
+                whos_turn = 0
+            whos_turn_label.text = "It is player %ss turn"%(whos_turn+1)
+            whos_turn_label.theme.label["text-color"] = world.players[whos_turn].color
+            whos_turn_label.make_image()
+
+        if world.one_winner()[0]:
+            finish_label = gui.Label(app, (-1, 375), "Finish Label", "Player %s WON!"%(world.one_winner()[1]+1),
+                          widget_pos="center")
+            finish_label.theme.label["text-color"] = world.players[world.one_winner()[1]].color
+            finish_label.theme.label["font"] = pygame.font.Font(None, 45)
+        #BATTLES!
         if len(picktwo) == 2:
-            if util.connected_mass(picktwo[0][1].terr, picktwo[1][1].terr):
-                if not picktwo[0][0] == picktwo[1][0]:
-                    x, y = rules.perform_battle(picktwo[0][1], picktwo[1][1])
-                    picktwo[0][1].units -= x
-                    picktwo[1][1].units -= y
-                    if picktwo[1][1].units == 0:
-                        world.players[picktwo[1][0]].territories.remove(picktwo[1][1])
-                        world.players[picktwo[0][0]].territories.append(picktwo[1][1])
+            if picktwo[0][0] == whos_turn:
+                if util.connected_mass(picktwo[0][1].terr, picktwo[1][1].terr):
+                    if not picktwo[0][0] == picktwo[1][0]:
+                        if picktwo[0][1].can_move:
+                            x, y = rules.perform_battle(picktwo[0][1], picktwo[1][1])
+                            picktwo[0][1].units -= x
+                            picktwo[1][1].units -= y
+                            if picktwo[1][1].units == 0:
+                                world.players[picktwo[1][0]].territories.remove(picktwo[1][1])
+                                world.players[picktwo[0][0]].territories.append(picktwo[1][1])
+                                if world.players[picktwo[1][0]].territories == []:
+##                                    dead.append(picktwo[1][0])
+                                    world.players[picktwo[1][0]].dead = True
 
-                        picktwo[1][1].units = picktwo[0][1].units - 1
-                        picktwo[0][1].units = 1
-                    picktwo[0][1].update()
-                    picktwo[1][1].update()
+                                if picktwo[1][1].max_units > picktwo[0][1].units - 1:
+                                    picktwo[1][1].units = picktwo[0][1].units - 1
+                                    picktwo[0][1].units = 1
+                                else:
+                                    picktwo[1][1].units = picktwo[1][1].max_units
+                                    picktwo[0][1].units -= picktwo[1][1].units
+                            picktwo[0][1].update()
+                            picktwo[1][1].update()
 
-                    print "casualties: %s, %s"%(x, y)
-                else:
-                    print "player territories - moving units"
-                    x = picktwo[0][1].units - 1
-##                    picktwo[0][1].units -= x
-##                    picktwo[1][1].units += x
-                    if picktwo[1][1].max_units >= picktwo[1][1].units + x:
-                        pass
+                            print "casualties: %s, %s"%(x, y)
+                        else:
+                            print "%s cannot move this turn!"%picktwo[0][1]
                     else:
-                        x = picktwo[1][1].max_units - picktwo[1][1].units
-                    picktwo[0][1].units -= x
-                    picktwo[1][1].units += x
-                    picktwo[0][1].update()
-                    picktwo[1][1].update()
+                        print "player territories - moving units"
+                        x = picktwo[0][1].units - 1
+                        if picktwo[1][1].max_units >= picktwo[1][1].units + x:
+                            pass
+                        else:
+                            x = picktwo[1][1].max_units - picktwo[1][1].units
+                        picktwo[0][1].units -= x
+                        picktwo[1][1].units += x
+                        picktwo[0][1].update()
+                        picktwo[1][1].update()
+                        picktwo[1][1].can_move = False
 
+                else:
+                    print "not touching territories"
             else:
-                print "not touching territories"
+                print "it is player %ss turn!"%whos_turn
 
             picktwo = []
 
@@ -145,4 +187,6 @@ def main():
                           pos[1] * world.tile_size[1] - world.offset[1],
                           world.tile_size[0], world.tile_size[1]),
                          1)
+
+        app.render()
         pygame.display.flip()
