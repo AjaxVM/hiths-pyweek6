f7c50ba45d10973ed5ab7ab3ae76e3a83deaa791
RoeBros <RoeBros@a921e572-4749-0410-9c32-5fb0c26aa395>
2008-04-04 03:34:28 +0000

    more network stuff.

--- trunk/net-tests/game_client1.py
+++ trunk/net-tests/game_client1.py
@@ -1,11 +1,10 @@
 import net, socket
-import random
 
 class Client(object):
     def __init__(self, username=""):
         self.username = username
         self.bad_username = False
-        self.game_name = None
+        self.game_name = "lobby"
 
         self.server = None #this will be the main server eventually
         self.port = 12345
@@ -23,48 +22,50 @@ class Client(object):
             #we've been rejected -- to many users!!!
             print "too many users connected to main server - please come back later"
             raise SystemExit()
-##            return False
         else:
-            a = net.request(self.server, ["USER_NAME", self.username]).data
+            a = net.request(self.server, ["USER_JOIN", self.username, self.server]).data
             if a == "BAD USER NAME":
                 #prompt user to pick a new username
                 self.bad_username = True
-##                return False
-##            return True
 
     def is_my_turn(self):
-        return net.request(self.server, net.Packet(["GET_WHOS_TURN", self.game_name])).data == self.my_player
+        return net.request(self.server, net.Packet(["GET_WHOS_TURN",
+                                                    self.game_name,
+                                                    self.username])).data == self.my_player
 
     def get_who_turn(self):
-        return net.request(self.server, net.Packet(["GET_WHOS_TURN", self.game_name])).data
+        return net.request(self.server, net.Packet(["GET_WHOS_TURN",
+                                                    self.game_name,
+                                                    self.username])).data
 
     def update_world(self, terr1, terr2, t1_troops, t2_troops, conquer=False):
         net.request(self.server, net.Packet(["UPDATE_WORLD", self.game_name,
+                                             self.username,
                                              terr1, terr2,
                                              t1_troops, t2_troop, conquer]))
 
     def get_messages(self):
-        return net.request(self.server, net.Packet(["GET_MESSAGES", self.game_name])).data
+        return net.request(self.server, net.Packet(["GET_MESSAGES", self.username])).data
 
     def send_message(self, message):
-        net.request(self.server, net.Packet(["MESSAGE", self.game_name, message]))
+        net.request(self.server, net.Packet(["MESSAGE", self.game_name, self.username, message]))
 
     def start_new_game(self, gamename, num_players):
-        a = net.requet(self.server, net.Packet(["NEW_GAME", gamename, num_players, self.username]))
+        a = net.requet(self.server, net.Packet(["NEW_GAME", gamename, self.username, num_players]))
         if a == "BAD GAME NAME":
             return False
 
         self.game_name = gamename
         return True
 
-    def shuffle_players(self):
-        a = net.request(self.server, net.Packet(["GET_NUM_PLAYERS", self.gamename])).data
-        cur = []
-        for i in xrange(a):
-            cur.append(i)
+    def end_turn(self):
+        net.request(self.server, net.Packet(["END_TURN", self.gamename, self.username])
 
-        random.shuffle(cur)
-        return cur
+    def get_num_players(self):
+        return net.request(self.server, net.Packet(["GET_NUM_PLAYERS", self.gamename, self.username])).data
+
+    def get_players(self):
+        return net.request(self.server, net.Packet(["GET_PLAYERS", self.gamename, self.username])).data
 
     def join_game(self, gamename): #join a pre-existing game
         a = net.request(self.server, net.Packet(["JOIN_GAME", gamename, self.username])).data
