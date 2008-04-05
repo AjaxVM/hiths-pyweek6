141100aeca8ac2cf61b326915c2ef3e89e108478
archwyrm <archwyrm@a921e572-4749-0410-9c32-5fb0c26aa395>
2008-04-05 19:51:33 +0000

    Implemented chat, disconnects, and some error handling. ConnectionRefusedError is still problematic

--- lib/tclient.py
+++ lib/tclient.py
@@ -2,8 +2,11 @@ from twisted.spread import pb
 from twisted.internet import reactor
 from tserver import User, CopyUser
 
+import sys, signal
+
 player_count = 0
-connect = None
+loop = 0
+connection = None
 client = None
 
 class Client(pb.Referenceable):
@@ -11,18 +14,21 @@ class Client(pb.Referenceable):
         self.name = name
         self.players = []
 
-    def connected(self, obj):
+    def connected(self, remote_ref):
         """Connected, send information and a remote reference"""
-        self.server = obj
+        self.server = remote_ref
         self.server.callRemote("join", self, self.name)
         main_loop()
 
     def remote_send_players(self, data):
         self.players = data
 
+    def remote_send_chat_msg(self, name, msg):
+        print name, "said:", msg
+
     def handle_connect_error(self, error):
         print "Error connecting: " + str(error.value)
-        connect.disconnect()
+        #connection.disconnect()
         reactor.stop()
 
 class RemoteUser(User, pb.RemoteCopy):
@@ -39,21 +45,44 @@ def main_loop():
         print "Names ",
         for i in client.players:
             print i.name,
-        print '' # Flush
+        sys.stdout.flush()
+        #print '' # Flush
         player_count = cur_players
 
+    #msg = input("Chat: ")
+    if loop == 0:
+        msg = 'Hello!'
+        client.server.callRemote("get_chat_msg", client, msg)
+
+    loop += 1
     reactor.callLater(0, main_loop)
+
+def handle_signal(signal, frame):
+    def disconnect(ref=None):
+        reactor.stop()
+
+    # We haven't made a connection yet
+    if not 'server' in client.__dict__:
+        disconnect()
+
+    try:
+        d = client.server.callRemote("leave", client)
+        d.addCallback(disconnect)
+    except pb.DeadReferenceError:
+        disconnect()
 
 def connect(name, host, port):
     global client
 
     client = Client(name)
     factory = pb.PBClientFactory()
-    myd = reactor.connectTCP(host, port, factory)
+    connection = reactor.connectTCP(host, port, factory)
     d = factory.getRootObject()
     d.addCallback(client.connected)
     d.addErrback(client.handle_connect_error)
     reactor.run()
+
+signal.signal(signal.SIGINT, handle_signal)
 
 if __name__ == '__main__':
     connect('markus', 'localhost', 10101) # TODO remove
